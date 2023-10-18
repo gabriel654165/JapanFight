43940171ebfb6decd20af6127c87834f274aa7a8
Gabriel <gabriel.medoukali@epitech.eu>
2023-10-18 15:58:35 +0200

    feat: new spaceship level scene and the metadata

--- Assets/Scripts/MapMetaData/MapMetaData.cs
+++ Assets/Scripts/MapMetaData/MapMetaData.cs
@@ -12,6 +12,9 @@ public class MapMetaData : MonoBehaviour
         new Tuple<int, Vector3>(0, new Vector3(-40, 1, 50)),
         new Tuple<int, Vector3>(1, new Vector3(28, 1, 75)),
         new Tuple<int, Vector3>(1, new Vector3(21, 1, 113)),
+
+        new Tuple<int, Vector3>(2, new Vector3(-21, 1, 1)),
+        new Tuple<int, Vector3>(2, new Vector3(-35, 1, 10)),
     };
 
     private Tuple<int, Vector3>[] m_spawnPosP2 = new Tuple<int, Vector3>[] {
@@ -19,6 +22,9 @@ public class MapMetaData : MonoBehaviour
         new Tuple<int, Vector3>(0, new Vector3(-35, 1, 45)),
         new Tuple<int, Vector3>(1, new Vector3(28, 1, 67)),
         new Tuple<int, Vector3>(1, new Vector3(12, 1, 113)),
+
+        new Tuple<int, Vector3>(2, new Vector3(-21, 1, 7)),
+        new Tuple<int, Vector3>(2, new Vector3(-30, 1, 10)),
     };
 
     private Tuple<int, Vector3>[] m_spawnPosCam = new Tuple<int, Vector3>[] {
@@ -26,6 +32,9 @@ public class MapMetaData : MonoBehaviour
         new Tuple<int, Vector3>(0, new Vector3(-37.5f, 0, 47.5f)),
         new Tuple<int, Vector3>(1, new Vector3(76, 4, 71)),
         new Tuple<int, Vector3>(1, new Vector3(16.5f, 4, 80)),
+        
+        new Tuple<int, Vector3>(2, new Vector3(-25, 1, 4)),
+        new Tuple<int, Vector3>(2, new Vector3(-32.5f, 1, 12)),
     };
 
     private Tuple<int, Vector3>[] m_cameraOffset = new Tuple<int, Vector3>[] {
@@ -33,6 +42,9 @@ public class MapMetaData : MonoBehaviour
         new Tuple<int, Vector3>(0, new Vector3(-20, 2.5f, -20)),
         new Tuple<int, Vector3>(1, new Vector3(-30, 2.5f, 0)),
         new Tuple<int, Vector3>(1, new Vector3(0, 2.5f, 25)),
+
+        new Tuple<int, Vector3>(2, new Vector3(-25, 1, 0)),
+        new Tuple<int, Vector3>(2, new Vector3(0, 1, -20)),
     };
 
     // @note: should be overriden
